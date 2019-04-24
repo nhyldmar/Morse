c95026f4d1135888548261f72ec348d1f331c3be
Nicholas Hyldmar <nh490@cam.ac.uk>
2019-04-24 16:16:26 +0100

    Fixed issues

--- morse_generator.py
+++ morse_generator.py
@@ -5,19 +5,6 @@ import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('-m', '--message', nargs='+', type=str, default='Hello World', help='Message')
-parser.add_argument('-n', '--filename', type=str, default='morse_out', help='File name')
-parser.add_argument('-f', '--frequency', type=int, default=1000, help='Frequency of tone in Hz')
-parser.add_argument('-t', '--time_unit', type=float, default=0.2, help='Length of a dot in seconds')
-args = parser.parse_args()
-
-message = ' '.join(args.message)
-filename = args.filename + '.wav'
-time_unit = args.time_unit
-tone_freq = args.frequency
-audio_rate = 44100
-
 # Morse translator; 0 = dot, 1 = dash
 morse = {'a': (0, 1),
          'b': (1, 0, 0, 0),
@@ -56,42 +43,88 @@ morse = {'a': (0, 1),
          '9': (1, 1, 1, 1, 0),
          '0': (1, 1, 1, 1, 1)}
 
-signal = []
-for letter in message:
-    if letter == ' ':
-        signal.extend((0, 0, 0, 0, 0))  # Seven units between words (two from below)
-        continue
 
-    try:
-        letter_triggers = morse[letter.lower()]
-    except KeyError:
-        raise ValueError("{} is not an approved character".format(letter))
+def get_signal(message):
+    """Generates the signal for a given word."""
+    signal = []
 
-    for dash in letter_triggers:
-        if dash:
-            signal.extend((1, 1, 1))  # Dash is three units
-        else:
-            signal.append(1)  # Dot is one unit
-        signal.append(0)  # One unit between triggers
-    signal.extend((0, 0))  # Three units between letters (one from after trigger)
+    for letter in message:
+        if letter == ' ':
+            signal.extend((0, 0, 0, 0, 0))  # Seven units between words (two from below)
+            continue
 
-chunk = time_unit * audio_rate  # Samples in one time unit
+        try:
+            letter_triggers = morse[letter.lower()]
+        except KeyError:
+            raise ValueError("{} is not an approved character".format(letter))
 
-tone_t_array = np.arange(0, time_unit, 1 / audio_rate)
-tone = np.sin(2 * np.pi * tone_freq * tone_t_array)
+        for dash in letter_triggers:
+            if dash:
+                signal.extend((1, 1, 1))  # Dash is three units
+            else:
+                signal.append(1)  # Dot is one unit
+            signal.append(0)  # One unit between triggers
+        signal.extend((0, 0))  # Three units between letters (one from after trigger)
 
-t_array = np.arange(0, len(signal) * time_unit, 1 / audio_rate)
-audio = []
+    return signal[:-3]
 
-for trigger in signal:
-    audio.extend(tone * trigger)
 
-# Save audio file as .wav
-audio = np.asarray(audio)
-audio *= 1000
-audio = np.asarray(audio, dtype=np.int16)
-wavfile.write(filename, audio_rate, audio)
+def get_waveform(signal):
+    """Generates the waveform for a given signal"""
+    signal = np.asarray(signal, dtype=np.int16)
 
-# Show waveform
-plt.plot(t_array, audio)
-plt.show()
+    chunk = time_unit * audio_rate  # Samples in one time unit
+
+    tone_t_array = np.arange(0, time_unit, 1 / audio_rate)
+    tone = np.sin(2 * np.pi * tone_freq * tone_t_array)
+
+    audio = []
+
+    for trigger in signal:
+        audio.extend(tone * trigger)
+
+    return audio
+
+
+def write_wav(audio, filename):
+    """Save audio file as .wav"""
+    audio = np.asarray(audio)
+    audio *= 1000
+    audio = np.asarray(audio, dtype=np.int16)
+    wavfile.write(filename, audio_rate, audio)
+
+
+def show_waveform(waveform):
+    """Plots the waveform"""
+    t_array = np.arange(0, len(waveform), 1)
+    plt.plot(t_array, waveform)
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Setup parser
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-m', '--message', nargs='+', type=str, default='E', help='Message to be processed')
+    parser.add_argument('-n', '--filename', type=str, default='morse_out', help='Name of file to audio save to')
+    parser.add_argument('-f', '--frequency', type=int, default=1000, help='Frequency of tone in Hz')
+    parser.add_argument('-t', '--time_unit', type=float, default=0.05, help='Length of a dot in seconds')
+    parser.add_argument('-s', '--show', nargs='?', const=True, default=False, help='Show the waveform')
+
+    # Process arguments
+    args = parser.parse_args()
+    message = ' '.join(args.message)
+    filename = args.filename + '.wav'
+    time_unit = args.time_unit
+    tone_freq = args.frequency
+    show = args.show
+    audio_rate = 44100
+
+    # Generate audio
+    signal = get_signal(message)
+    waveform = get_waveform(signal)
+    write_wav(waveform, filename)
+
+    # Show waveform
+    if show:
+        show_waveform(waveform)
